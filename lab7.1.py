dabd56063613dea4da120b14624478c631aa1031
siddharthmane28 <officialsiddharth84@gmail.com>
2026-07-30 13:59:37 +0530

    Add files via upload

---
+++ lab7.1.py
@@ -0,0 +1,10 @@
+n=int(input("Enter a number:"))
+
+if n<5:
+    print(n,"is less than 5")
+
+elif n>5:
+    print(n,"is greter then 5")
+
+else:
+    print(n,"is equal to 5")        
---
+++ lab7.py
@@ -0,0 +1,6 @@
+n=int(input("Enter The Number:"))
+if n<5:
+    print(n,"is less than 5")
+
+else:
+    print(n,"is not less than 5")    
