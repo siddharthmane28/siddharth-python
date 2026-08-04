976db8d67b5c6979c09a7c3c8cf53df8ba052212
siddharthmane28 <officialsiddharth84@gmail.com>
2026-08-04 12:52:13 +0530

    Add files via upload

---
+++ star.py
@@ -0,0 +1,13 @@
+print("======Star Pattern======")
+
+for i in range(1,6):
+    for j in range(i):
+     print("*",end=" ")
+    print()
+
+print()
+
+for i in range(1,6):
+   for j in range(1,i+1):
+      print(j,end=" ")
+   print()
